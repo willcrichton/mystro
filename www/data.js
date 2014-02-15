e2c92dcfb1941da46522ba2ad5b8f5c098956fa4
Philip Garrison <garip505@gmail.com>
2014-02-14 23:37:19 -0500

    Define data structure in data.js

--- www/data.js
+++ www/data.js
@@ -1,5 +1,42 @@
+var oldData = {
+    // key: value pairs where key is the time in ms
+    // 1234: {
+    //     pointerTip: ...,
+    //     pointerSpeed: ..., 
+    //     handLoc: ..., 
+    //     ...
+    //  }
+}
+
+function detectSelect(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
+
+}
+
+function detectVolumeChange(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
+
+}
+
+function detectTempoChange(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
+
+}
+
+
+function detectOrchLoc(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
+
+}
+
+// Called when the leapmotion gets new data.
+// pointerTip : array 0..2
+// pointerSpeed : int
+// handLoc : array 0..2
+// palmDir : array 0..2 (normalized)
+// fingerDir : array 0..2 (normalized)
+// For each input, no data if null
 function pushData(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
-    
+    detectSelect(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir);
+    detectVolumeChange(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir);
+    detectTempoChange(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir);
+    detectOrchLoc(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir);
 }
 
 exports.pushData = pushData;
