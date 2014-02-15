45fb8bb53e680d73c0e427d61478770cbb9c4aff
Patrick Xia <xiapatrick418@hotmail.com>
2014-02-15 00:57:47 -0500

    changed data.js

--- www/data.js
+++ www/data.js
@@ -29,8 +29,51 @@ function detectTempoChange(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir
 
 
 // Points to a region in the orchestra.
-function detectOrchLoc(pointerTip, pointerSpeed, handLoc, palmDir, fingerDir) {
 
+// Implementation: 
+
+function detectRecentHandLoc(){
+    var LEFTEDGE = -300; 
+    var TOPEDGE = 400;
+    return [LEFTEDGE, TOPEDGE];
+}
+
+function detectOrchLoc(handLoc, fingerDir) {
+    // Constants for determining edges
+
+    var RIGHTEDGE = 50;
+    var LEFTEDGE = -300; 
+    var BOTTOMEDGE = 200;
+    var TOPEDGE = 400;
+    var DEPTH =  50;         //variable
+
+    // USE OLD DATA 
+    if(handLoc === NULL){
+	handLoc = detectRecentHandLoc()
+    }
+    var handX = handLoc[0];
+    var handY = handLoc[1];
+
+    if(fingerLoc === NULL){
+	fingerLoc === [0, 0, 1];
+    }
+
+    var fingerX = fingerLoc[0];
+    var fingerY = fingerLoc[1];
+    var fingerNorm = Math.sqrt(fingerX, fingerX + fingerY+fingerY);
+    if(fingerNorm === 0){
+	fingerLocNorm = 0;
+    }
+    else{
+	var fingerLocNorm = [fingerX/, 
+			      fingerY/(Math.sqrt(fingerX*fingerX + fingerY+fingerY))];
+    }
+
+    var finalHandLoc = [fingerX + fingerLocNorm*DEPTH, fingerY + fingerLocNorm*DEPTH];
+    var finalNormedLoc = [(fingerHandLoc[0] - LEFTEDGE)/(RIGHTEDGE-LEFTEDGE), 
+			  (fingerHandLoc[1] - BOTTOMEDGE)/(TIOEDGE-BOTTOMEDGE)];
+
+    return finalNormedLoc;
 }
 
 // Called when the leapmotion gets new data.
