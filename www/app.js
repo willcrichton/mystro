ea7ba46414e0e639629c90f242e645ff444b327c
Dillon Lareau <Dillon@Dillons-MacBook-Pro-2.local>
2014-02-14 21:49:14 -0500

    Simple point tracking

--- www/app.js
+++ www/app.js
@@ -4,6 +4,22 @@ $(function() {
     swiper.update(function(g) {
         // do things here
     });
-    
+
+    var stabilizedDisplay = document.getElementById("stabPosition");
+    //var deltaDisplay = document.getElementById("delta");
+    ctl.on('frame', function(frame){
+	if(frame.pointables.length > 0)
+	{
+            var pointable = frame.pointables[0];
+            var stabilizedPosition = pointable.stabilizedTipPosition;
+            var tipPosition = pointable.tipPosition;
+            stabilizedDisplay.innerText = "(" + Math.round(stabilizedPosition[0]) + ", " 
+                + Math.round(stabilizedPosition[1]) + ", " 
+                + Math.round(stabilizedPosition[2]) + ")";
+        //    deltaDisplay.innerText = "(" + (tipPosition[0] - stabilizedPosition[0]) + ", "
+        //        + (tipPosition[1] - stabilizedPosition[1]) + ", "
+        //        + (tipPosition[2] - stabilizedPosition[2]) + ")";
+	}
+    });
     ctl.connect();
 });
